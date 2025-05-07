e6e43868a3d56d4693876929117e1d268aa076fc
MVBeats <real9ms@gmail.com>
2025-05-07 11:56:36 +0200

    Setup: Express-Server mit Basis-Konfiguration

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('API läuft!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+});
